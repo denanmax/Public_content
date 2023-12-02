02bcc3750f4273df6632c4c270f8c093d46d5c94
denanmax <deshis93@gmail.com>
2023-12-02 15:19:39 +0300

    urls

--- config/urls.py
+++ config/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls', namespace='main')),
     path('users/', include('users.urls', namespace='users')),
+    path('posts/', include('posts.urls', namespace='posts')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
