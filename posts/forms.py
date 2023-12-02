56c4d5e1d245d2ccffa34eea43b880d46f3e53de
denanmax <deshis93@gmail.com>
2023-12-02 14:56:35 +0300

    posts forms

--- posts/forms.py
+++ posts/forms.py
@@ -8,8 +8,26 @@ class AddPostMyForm(forms.ModelForm):
         model = Posts
         fields = ('title', 'content', 'image', 'is_published', 'paid_published', 'cost')
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["title"].widget.attrs.update({"class": "form-control"})
+        self.fields["content"].widget.attrs.update({"class": "form-control"})
+        self.fields["image"].widget.attrs.update({"class": "form-control"})
+        self.fields["is_published"].widget.attrs.update({"class": "form-check-input"})
+        self.fields["paid_published"].widget.attrs.update({"class": "form-check-input"})
+        self.fields["cost"].widget.attrs.update({"class": "form-control"})
+
 
 class UpdatePostMyForm(forms.ModelForm):
     class Meta:
         model = Posts
         fields = ('title', 'content', 'image', 'is_published', 'paid_published', 'cost')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["title"].widget.attrs.update({"class": "form-control"})
+        self.fields["content"].widget.attrs.update({"class": "form-control"})
+        self.fields["image"].widget.attrs.update({"class": "form-control"})
+        self.fields["is_published"].widget.attrs.update({"class": "form-check-input"})
+        self.fields["paid_published"].widget.attrs.update({"class": "form-check-input"})
+        self.fields["cost"].widget.attrs.update({"class": "form-control"})
