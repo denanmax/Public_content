b63e1a762c75dc07b53e07e2aba4cafdb2ec3b00
denanmax <deshis93@gmail.com>
2023-12-03 11:41:06 +0300

    model post updt

--- posts/models.py
+++ posts/models.py
@@ -1,13 +1,14 @@
 from django.conf import settings
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.urls import reverse
 
 from users.models import NULLABLE
 
 
-# Create your models here.
 class Posts(models.Model):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, **NULLABLE)
-    title = models.CharField(max_length=100, verbose_name='Заголовок')
+    title = models.CharField(max_length=15, verbose_name='Заголовок')
     content = models.TextField(verbose_name='Cодержимое')
     image = models.ImageField(upload_to='posts/', verbose_name='Изображение', **NULLABLE)
     create_date = models.DateTimeField(verbose_name='Дата создания', auto_now_add=True)
@@ -21,7 +22,14 @@ class Posts(models.Model):
     def __str__(self):
         return self.title
 
+    def get_absolute_url(self):
+        return reverse('posts:posts', args=[str(self.id)])
+
+    def clean(self) -> None:
+        super().clean()
+        if self.cost < 100:
+            raise ValidationError({'cost': 'Цена публикации не может быть ниже 100 рублей.'})
+
     class Meta:
         verbose_name = 'Пост'
         verbose_name_plural = 'Посты'
-        ordering = ['-create_date']
